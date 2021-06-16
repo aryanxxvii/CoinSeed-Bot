cde709f90385e50f4f359ebd9d00e9edb963b6ab
aryanxxvii <53692529+aryanxxvii@users.noreply.github.com>
2021-06-16 19:49:10 +0530

    Delete demoji.py

--- demoji.py
+++
@@ -1,7 +0,0 @@
-import emoji
-
-string = ":fishing_pole:"
-demoji = emoji.demojize("🍑 ")   # This takes the emoji and gives--> :fishing_pole:
-emoji = emoji.emojize(string)    # This takes :fishing_pole: and gives--> :fishing_pole_and_fish:    
-print(demoji)
-print(emoji)
