b7ba8df0c8e2b2a21d1a440f1b56002a12d6e2e8
Warpidee <85791415+Warpidee@users.noreply.github.com>
2021-06-16 19:38:13 +0530

    Update sqlfunc.py

--- sqlfunc.py
+++ sqlfunc.py
@@ -1,6 +1,6 @@
 from re import X
 import mysql.connector
-mydb = mysql.connector.connect(host="johnny.heliohost.org", user="aryan27_coinseedbot", passwd="Aryanxxvii27!", database="aryan27_coinseed", charset="utf8mb4", use_unicode = "True")
+mydb = mysql.connector.connect(host="johnny.heliohost.org", user="aryan27_coinseedbot", passwd="Aryanxxvii27!", database="aryan27_coinseed")
 mycursor = mydb.cursor()
 
 def sql_add(tablename, key=None, rec = list()):
