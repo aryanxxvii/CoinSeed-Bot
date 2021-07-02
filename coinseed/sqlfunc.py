be63c1a285e8f0a0f55c77fee26e3317ab837b34
aryanxxvii <53692529+aryanxxvii@users.noreply.github.com>
2021-07-02 17:29:21 +0530

    aaa

--- coinseed/sqlfunc.py
+++ coinseed/sqlfunc.py
@@ -129,7 +129,8 @@ def sql_server_topusers(guid): #list the players in the server, csym
     mycursor.execute("SELECT CSYM, CNAM FROM DGUILDS WHERE GUID = {}".format(int(guid)))
     csym, cnam = mycursor.fetchone()
     mycursor.execute("SELECT DUID, CBAL FROM DUSERS WHERE GUID = {} ORDER BY CBAL".format(int(guid)))
-    user_list = mycursor.fetchall()[:10:-1]
+    user_list = mycursor.fetchall()[:10]
+    user_list = user_list[::-1]
     return user_list, csym, cnam
 
 def sql_user_cngserver(duid, newguid):
